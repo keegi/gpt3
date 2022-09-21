a70f5f55fffdbd68c57b15b48b2542b157a59e48
ck kim <keegi@kakao.com>
2022-09-21 18:30:09 +0900

    header Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from "react";
-import './App.css';
+import React from 'react';
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
-import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { CTA, Brand, Navbar } from './components';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="gradient__bg">
-         <Navbar />
-         <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
